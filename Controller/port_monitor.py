8e6ac34098c193453aa28002450502219ab1e226
Kundan Veluru <kundanveluru@gmail.com>
2026-04-09 21:38:42 +0530

    Add files via upload

---
+++ Controller/port_monitor.py
@@ -0,0 +1,46 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+
+class PortMonitor(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(PortMonitor, self).__init__(*args, **kwargs)
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_FLOOD)]
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+
+        mod = parser.OFPFlowMod(datapath=datapath, priority=0,
+                               match=match, instructions=inst)
+        datapath.send_msg(mod)
+
+    @set_ev_cls(ofp_event.EventOFPPortStatus, MAIN_DISPATCHER)
+    def port_status_handler(self, ev):
+        msg = ev.msg
+        reason = msg.reason
+        port_no = msg.desc.port_no
+
+        ofproto = msg.datapath.ofproto
+
+        if reason == ofproto.OFPPR_ADD:
+            print(f"[INFO] Port {port_no} ADDED")
+
+        elif reason == ofproto.OFPPR_DELETE:
+            print(f"[ALERT] Port {port_no} DELETED")
+
+        elif reason == ofproto.OFPPR_MODIFY:
+            if msg.desc.state:
+                print(f"[ALERT] Port {port_no} DOWN")
+            else:
+                print(f"[INFO] Port {port_no} UP")
