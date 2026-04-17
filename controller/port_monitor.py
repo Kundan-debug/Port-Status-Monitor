fe61bc3e1b408154b42d9dbafce8b02ef17b2a74
Kundan Veluru <kundanveluru@gmail.com>
2026-04-17 20:04:55 +0530

    Refactor port_monitor.py for improved readability

--- controller/port_monitor.py
+++ controller/port_monitor.py
@@ -1,6 +1,6 @@
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 
@@ -9,6 +9,7 @@ class PortMonitor(app_manager.RyuApp):
 
     def __init__(self, *args, **kwargs):
         super(PortMonitor, self).__init__(*args, **kwargs)
+        self.port_status = {}
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
@@ -19,28 +20,52 @@ class PortMonitor(app_manager.RyuApp):
         match = parser.OFPMatch()
         actions = [parser.OFPActionOutput(ofproto.OFPP_FLOOD)]
 
-        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        inst = [parser.OFPInstructionActions(
+            ofproto.OFPIT_APPLY_ACTIONS, actions)]
 
-        mod = parser.OFPFlowMod(datapath=datapath, priority=0,
-                               match=match, instructions=inst)
+        mod = parser.OFPFlowMod(
+            datapath=datapath,
+            priority=0,
+            match=match,
+            instructions=inst
+        )
+
         datapath.send_msg(mod)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        in_port = msg.match['in_port']
+
+        actions = [parser.OFPActionOutput(ofproto.OFPP_FLOOD)]
+
+        out = parser.OFPPacketOut(
+            datapath=datapath,
+            buffer_id=msg.buffer_id,
+            in_port=in_port,
+            actions=actions,
+            data=msg.data
+        )
+
+        datapath.send_msg(out)
 
     @set_ev_cls(ofp_event.EventOFPPortStatus, MAIN_DISPATCHER)
     def port_status_handler(self, ev):
         msg = ev.msg
         reason = msg.reason
         port_no = msg.desc.port_no
-
         ofproto = msg.datapath.ofproto
 
-        if reason == ofproto.OFPPR_ADD:
-            print(f"[INFO] Port {port_no} ADDED")
-
-        elif reason == ofproto.OFPPR_DELETE:
-            print(f"[ALERT] Port {port_no} DELETED")
-
-        elif reason == ofproto.OFPPR_MODIFY:
-            if msg.desc.state:
-                print(f"[ALERT] Port {port_no} DOWN")
+        if reason == ofproto.OFPPR_MODIFY:
+            if msg.desc.state & ofproto.OFPPS_LINK_DOWN:
+                if self.port_status.get(port_no) != "DOWN":
+                    print(f"[ALERT] Port {port_no} DOWN")
+                    self.port_status[port_no] = "DOWN"
             else:
-                print(f"[INFO] Port {port_no} UP")
+                if self.port_status.get(port_no) != "UP":
+                    print(f"[INFO] Port {port_no} UP")
+                    self.port_status[port_no] = "UP"
